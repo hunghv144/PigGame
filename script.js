6d640fceca2820cead95c96eaf24f10995642a38
hunghv144 <122261032+hunghv144@users.noreply.github.com>
2023-02-16 16:11:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+"use strict";
+
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// Băt đầu
+// score0El.textContent = 0;
+// score1El.textContent = 0;
+// diceEl.classList.add("hidden");
+
+// let scores = [0, 0];
+// let currentScore = 0;
+// let activePlayer = 0;
+// let playing = true;
+
+let scores, currentScore, activePlayer, playing;
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+  diceEl.classList.add("hidden");
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+};
+init();
+// Nút đổi người chơi
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
+// Quay xúc xắc
+
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //console.log(dice);
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+    // Kiểm tra xem xúc xăc có bằng 1 hay không nếu bàng 1 chuyển sang người chơi tiếp theo
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    }
+    // Chuyển sang người chơi tiếp theo
+    else {
+      // document.getElementById(`current--${activePlayer}`).textContent = 0;
+      // currentScore = 0;
+      // activePlayer = activePlayer === 0 ? 1 : 0;
+      // player0El.classList.toggle("player--active");
+      // player1El.classList.toggle("player--active");
+      switchPlayer();
+    }
+  }
+});
+// Tạo nút Hold score
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    //1. Công điểm hiện tại vào điểm người chơi dang hoạt động
+    scores[activePlayer] += currentScore;
+    //scores[1] = scores[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    //2.Kiểm tra xem điểm >=100 thì kết thúc trò chơi
+    if (scores[activePlayer] >= 20) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+      playing = false; // Kết thúc trò chơi
+      diceEl.classList.add("hidden");
+    } else {
+      //3. Điểm < 100 chuyển sang người tiếp theo
+      switchPlayer();
+    }
+  }
+});
+// Tạo nút New Game
+btnNew.addEventListener("click", init); //function () {
+//   score0El.textContent = 0;
+//   score1El.textContent = 0;
+//   current0El.textContent = 0;
+//   current1El.textContent = 0;
+
+//   player0El.classList.remove("player--winner");
+//   player1El.classList.remove("player--winner");
+//   player0El.classList.add("player--active");
+//   player1El.classList.remove("player--active");
+//   diceEl.classList.add("hidden");
+
+//   scores = [0, 0];
+//   currentScore = 0;
+//   activePlayer = 0;
+//   playing = true;
+// });
